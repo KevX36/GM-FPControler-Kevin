5ca818f862c2cf8f437e0bdd473056bbec4ba823
KevX36 <kevincard2007@gmail.com>
2025-11-21 18:48:41 -0400

    got camera working, need to make movement match camera

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -12,50 +14,98 @@ public class PlayerMove : MonoBehaviour
 
 
     }
-    
+
     public float accelerate = 0.5f;
     public float deccelerate = 0.5f;
     public float topSpeed = 10;
+    public float runSpeed = 20;
     public float speed = 1f;
     private float basespeed;
     CharacterController controller;
+    public GameObject cam;
+    private float pitch = 0;
+    private float yaw =0;
+    public float pitchMax = 60;
+    public float pitchMin = -60;
+
+
     // Update is called once per frame
     void Update()
     {
+        
+        
+        
+        
         //sprint
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed += accelerate;
-            if (speed > topSpeed)
+            if (speed > runSpeed)
             {
-                speed = topSpeed;
+                speed = runSpeed;
             }
         }
         else if (Input.GetKey(KeyCode.RightShift))
         {
             speed += accelerate;
-            if (speed > topSpeed)
+            if (speed > runSpeed)
             {
-                speed = topSpeed;
+                speed = runSpeed;
             }
         }
         else if (speed > basespeed)
         {
-            speed-=deccelerate;
+            speed -= deccelerate;
             if (speed < basespeed)
             {
                 speed = basespeed;
             }
         }
 
-        
+
 
         //movement
-        
+
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
 
+
+        controller.Move(moveDirection * speed * Time.deltaTime);
+
+
+        //camera
+        pitch += Input.GetAxis("Mouse Y");
+        if (pitch < pitchMin)
+        {
+            pitch = pitchMin;
+        }
+        if (pitch > pitchMax)
+        {
+            pitch = pitchMax;
+        }
+
+        yaw += Input.GetAxis("Mouse X");
         
-        controller.Move(moveDirection*speed*Time.deltaTime);
+        cam.transform.rotation = Quaternion.Euler(-pitch, yaw, 0);
+        transform.rotation = Quaternion.Euler(0, yaw, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+    
+    
+
 }
